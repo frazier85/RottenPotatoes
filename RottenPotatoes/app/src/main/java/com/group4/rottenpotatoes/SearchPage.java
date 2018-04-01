62166ca1f7d8beef93299804bad2ad38cab1484c
Matthew Frazier <frazier85@gmail.com>
2018-04-01 17:28:54 -0400

    More front end UI work for app

--- RottenPotatoes/app/src/main/java/com/group4/rottenpotatoes/SearchPage.java
+++ RottenPotatoes/app/src/main/java/com/group4/rottenpotatoes/SearchPage.java
@@ -1,16 +1,20 @@
 package com.group4.rottenpotatoes;
 
+import android.app.ListActivity;
+import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class SearchPage extends AppCompatActivity {
+public class SearchPage extends ListActivity {
 
     Button mHomeButton;
     Button mSearchButton;
     Button mLoginRegisterButton;
+    ListView mSearchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -20,6 +24,13 @@ public class SearchPage extends AppCompatActivity {
         mHomeButton = findViewById(R.id.homeButton);
         mSearchButton = findViewById(R.id.searchButton);
         mLoginRegisterButton = findViewById(R.id.loginRegisterButton);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
 
         mLoginRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,5 +47,12 @@ public class SearchPage extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    private void doMySearch(String query) {
+        // TODO: Perform the search
+        // Return search results with an Adapter?
+        // https://developer.android.com/guide/topics/search/search-dialog.html
+
     }
 }
