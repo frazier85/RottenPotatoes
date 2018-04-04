1c51cccaebbb00b548f98448a2852ab941234b99
Matthew Frazier <frazier85@gmail.com>
2018-04-04 15:09:01 -0400

    Added some work on search functionality. Probably doesn't work properly yet

--- RottenPotatoes/app/src/main/java/com/group4/rottenpotatoes/SearchPage.java
+++ RottenPotatoes/app/src/main/java/com/group4/rottenpotatoes/SearchPage.java
@@ -1,13 +1,27 @@
 package com.group4.rottenpotatoes;
 
+import android.app.DownloadManager;
 import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class SearchPage extends ListActivity {
 
@@ -15,6 +29,11 @@ public class SearchPage extends ListActivity {
     Button mSearchButton;
     Button mLoginRegisterButton;
     ListView mSearchView;
+
+    private static final String URL = "http://project.codethree.net//api/search.php?by=";
+    List<Song> songList;
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -55,6 +74,44 @@ public class SearchPage extends ListActivity {
         // Return search results with an Adapter?
         // https://developer.android.com/guide/topics/search/search-dialog.html
 
-        Adapter results;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //converting the string to json array object
+                            JSONArray array = new JSONArray(response);
+
+                            //traversing through all the object
+                            for (int i = 0; i < array.length(); i++) {
+
+                                //getting product object from json array
+                                JSONObject song = array.getJSONObject(i);
+
+                                //adding the product to product list
+                                songList.add(new Song(
+                                        song.getString("artist"),
+                                        song.getString("title"),
+                                        song.getString("link"),
+                                        song.getString("review"),
+                                        song.getString("genre")
+                                ));
+                            }
+
+                            //creating adapter object and setting it to recyclerview
+                            SongAdapter adapter = new SongAdapter(SearchPage.this, songList);
+                            recyclerView.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        Volley.newRequestQueue(this).add(stringRequest);
     }
 }
