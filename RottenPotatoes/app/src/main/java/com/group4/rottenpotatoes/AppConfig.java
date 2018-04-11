248c7422f7301abf05c7f5042227fbaa46584669
Terrence <terrencehlight@gmail.com>
2018-04-11 14:35:03 -0400

    Login/Register functionality

---
+++ RottenPotatoes/app/src/main/java/com/group4/rottenpotatoes/AppConfig.java
@@ -0,0 +1,10 @@
+package com.group4.rottenpotatoes;
+
+public class AppConfig {
+
+        // Server user login url
+        public static String URL_LOGIN = "http://project.codethree.net/api/user.php?action=login";
+
+        // Server user register url
+        public static String URL_REGISTER = "http://project.codethree.net/api/user.php?action=register";
+}
--- RottenPotatoes/app/src/main/java/com/group4/rottenpotatoes/RegisterPage.java
+++ RottenPotatoes/app/src/main/java/com/group4/rottenpotatoes/RegisterPage.java
@@ -1,5 +1,26 @@
 package com.group4.rottenpotatoes;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
@@ -15,204 +36,180 @@ import android.widget.EditText;
 
 import java.util.Objects;
 
-public class RegisterPage extends AppCompatActivity {
-
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private RegisterPage.UserRegisterTask mRegisterTask= null;
-
-    // UI references.
-    private EditText mFullNameView;
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private EditText mPasswordConfirmView;
-    private EditText mUserTypeView;
-    private View mProgressView;
-    private View mRegisterFormView;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
+public class RegisterPage extends Activity {
+    private static final String TAG = RegisterPage.class.getSimpleName();
+    private Button btnRegister;
+    private Button btnLinkToLogin;
+    private EditText inputFirstName;
+    private EditText inputLastName;
+    private EditText inputEmail;
+    private EditText inputUsername;
+    private EditText inputPassword;
+    private ProgressDialog pDialog;
+    private SessionManager session;
+    private SQLiteHandler db;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
 
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(LoginPage.EXTRA_MESSAGE);
-    }
+        inputFirstName = (EditText) findViewById(R.id.firstName);
+        inputLastName = (EditText) findViewById(R.id.lastName);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputUsername = (EditText) findViewById(R.id.username);
+        inputPassword = (EditText) findViewById(R.id.password);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnLinkToLogin = (Button) findViewById(R.id.btnLinkToLoginScreen);
 
-    private void attemptRegister() {
-        if (mRegisterTask != null) {
-            return;
+        // Progress dialog
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+
+        // Session manager
+        session = new SessionManager(getApplicationContext());
+
+        // SQLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // Check if user is already logged in or not
+        if (session.isLoggedIn()) {
+            // User is already logged in. Take him to main activity
+            Intent intent = new Intent(RegisterPage.this,
+                    MainActivity.class);
+            startActivity(intent);
+            finish();
         }
 
-        // Reset errors.
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
+        // Register Button Click event
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String firstname = inputFirstName.getText().toString().trim();
+                String lastname = inputLastName.getText().toString().trim();
+                String email = inputEmail.getText().toString().trim();
+                String username = inputUsername.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
 
-        // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
-        String passwordConfirmation = mPasswordConfirmView.getText().toString();
-        String fullName = mFullNameView.getText().toString();
-        // This probably isn't a string. Need to figure out what it will be using radio buttons
-        String userType = mUserTypeView.getText().toString();
+                if (!firstname.isEmpty() && !lastname.isEmpty() && !email.isEmpty() && !username.isEmpty() && !password.isEmpty()) {
+                    registerUser(firstname, lastname, email, username, password);
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Please enter your details!", Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
 
-        boolean cancel = false;
-        View focusView = null;
+        // Link to Login Screen
+        btnLinkToLogin.setOnClickListener(new View.OnClickListener() {
 
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),
+                        LoginPage.class);
+                startActivity(i);
+                finish();
+            }
+        });
 
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
-        }
-
-        // Check if the password and the confirm password boxes are the same
-        if(!Objects.equals(password, passwordConfirmation)) {
-            // TODO: Find a better error message for this. Something indicating passwords not matching
-            mPasswordConfirmView.setError(getString(R.string.error_invalid_password));
-            cancel = true;
-        }
-
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-            mRegisterTask = new UserRegisterTask(email, password, fullName, userType);
-            mRegisterTask.execute((Void) null);
-        }
-    }
-
-    private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
-    }
-
-    private boolean isPasswordValid(String password) {
-        // TODO: Replace this with your own logic
-        return password.length() > 4;
     }
 
     /**
-     * Shows the progress UI and hides the login form.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+     * Function to store user in MySQL database will post params(tag, name,
+     * email, password) to register url
+     * */
+    private void registerUser(final String firstname, final String lastname, final String email,
+                              final String username, final String password) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_register";
 
-            mRegisterFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mRegisterFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mRegisterFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
+        pDialog.setMessage("Registering ...");
+        showDialog();
 
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        StringRequest strReq = new StringRequest(Method.POST,
+                AppConfig.URL_REGISTER, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+                    if (!error) {
+                        // User successfully stored in MySQL
+                        // Now store the user in sqlite
+                        String uid = jObj.getString("uid");
+
+                        JSONObject user = jObj.getJSONObject("user");
+                        int admin = 0;                                          //Cannot register as an admin, must be promoted
+                        String firstname = user.getString("firstname");
+                        String lastname = user.getString("lastname");
+                        String email = user.getString("email");
+                        String username = user.getString("username");
+                        String password = user.getString("password");
+
+                        // Inserting row in users table
+                        db.addUser(admin, firstname, lastname , email, username, password);
+
+                        Toast.makeText(getApplicationContext(), "User successfully registered. Try login now!", Toast.LENGTH_LONG).show();
+
+                        // Launch login activity
+                        Intent intent = new Intent(
+                                RegisterPage.this,
+                                LoginPage.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+
+                        // Error occurred in registration. Get the error
+                        // message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mRegisterFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Registration Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("firstname", firstname);
+                params.put("lastname", lastname);
+                params.put("email", email);
+                params.put("username", username);
+                params.put("password", password);
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
     }
 
-    public class UserRegisterTask extends AsyncTask<Void, Void, Boolean>
-    {
-
-        private final String mEmail;
-        private final String mPassword;
-        private final String mFullName;
-        private final String mUserType;
-
-        UserRegisterTask(String email, String password, String fullName, String userType)
-        {
-            // Basic constructor. Set member variables
-            mEmail = email;
-            mPassword = password;
-            mFullName = fullName;
-            mUserType = userType;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params)
-        {
-            // TODO: attempt authentication against a network service.
-
-            try
-            {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mRegisterTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mRegisterTask = null;
-            showProgress(false);
-        }
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
     }
 
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
 }
